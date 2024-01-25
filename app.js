d297b3f32d89b2051ebdae0604920d56fc9f80d7
smaasad <152586932+smaasad@users.noreply.github.com>
2024-01-25 03:21:39 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+document.getElementById(`h1`).innerText = `Hello Bangladesh!`;
+document.getElementById(`h1`).style.color =`red`;
+
+document.querySelector(`button`);
+addEventListener(`click`,function(){
+document.querySelector(`h1`).innerText = `Hello Bangladesh!`;
+document.querySelector(`h1`).style.color = `aqua`;
+
+})
